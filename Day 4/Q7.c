3d108ce3ceff071c8a0f0b5107db4606280821d1
Sahaj <sahaj1406.clg@gmail.com>
2025-08-24 14:24:14 +0530

    100DaysofCode

---
+++ Day 4/Q7.c
@@ -0,0 +1,21 @@
+// Write a program to swap two numbers without using a third variable.
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("first number= ");
+    scanf("%d",&a);
+    printf("second number= ");
+    scanf("%d",&b);
+
+    printf("before swapping, a=%d,b=%d\n", a ,b);
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    
+    printf("after swapping, a=%d,b=%d\n", a ,b);
+    return 0;
+
+
+
+
+}
---
+++ Day 4/Q8.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("enter number= ");
+    scanf("%d",&a);
+    int sum;
+    sum=a*(a+1)/2;
+    printf("sum= %d", sum);
+    return 0;
+
+
+
+
+}
